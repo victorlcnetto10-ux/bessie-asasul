db99a2d076ad9be5e5b848fa04a8b25eba310a27
victorlcnetto10-ux <victorlcnetto10@gmail.com>
2026-06-08 14:05:09 +0000

    Adicionar analytics e speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.tsx";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
 import "./index.css";
@@ -6,5 +8,7 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
     <App />
+    <Analytics />
+    <SpeedInsights />
   </ErrorBoundary>
 );
