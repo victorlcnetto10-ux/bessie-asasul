97b3e22c88af608c9eb2fc9593dc6b7fa506b280
victorlcnetto10-ux <victorlcnetto10@gmail.com>
2026-06-08 14:16:31 +0000

    Atualizar imports Vercel para next

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import App from "./App.tsx";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
 import "./index.css";
